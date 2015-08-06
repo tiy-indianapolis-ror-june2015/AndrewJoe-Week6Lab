747f14ac50ecd8a22334570e638bb269bca053db
andrewariley87 <andrewariley87@gmail.com>
2015-08-06 16:16:54 -0400

    follow gem
    set up the follow gem and added error message for Post Controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+
+  validate :body
+  validate :post_length
+  belongs_to :user
+
+
+  def post_length
+    errors.add(:body, "of post is too long, can't be longer than 170 characters") unless body.to_s.length < 170
+  end
+
 end
