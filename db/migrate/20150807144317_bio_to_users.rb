0e311506da029a9ae7ecb008586b28354f6d6660
andrewariley87 <andrewariley87@gmail.com>
2015-08-07 10:51:41 -0400

    wizard
    adding first steps to wizard

---
+++ db/migrate/20150807144317_bio_to_users.rb
@@ -0,0 +1,5 @@
+class BioToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :bio, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807132141) do
+ActiveRecord::Schema.define(version: 20150807144356) do
 
   create_table "follows", force: :cascade do |t|
     t.integer  "followable_id",                   null: false
