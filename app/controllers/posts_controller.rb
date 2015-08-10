16caf0c19e74974217e3a9a790ffa2eda60699d9
andrewariley87 <andrewariley87@gmail.com>
2015-08-10 14:06:05 -0400

    post_params
    only permitting a :body, no longer permitting a :user_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -93,6 +93,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:body,:user_id)
+      params.require(:post).permit(:body)
     end
 end
